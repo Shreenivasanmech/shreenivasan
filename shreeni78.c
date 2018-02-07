b0a54e5b15102323243efec25d1bf3bf8620fd08
Shreenivasanmech <35688877+Shreenivasanmech@users.noreply.github.com>
2018-02-07 16:19:39 +0530

    shree

---
+++ shreeni78.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,sum=0;
+printf("enter thwe natural number");
+scanf("%d"&n");
+for(a=0;a<=0;a++)
+{
+printf("sum of natural number");
+}
+getch();
+return 0;
+}
