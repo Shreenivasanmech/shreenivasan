bb0be6264e07b89e44e2b538b6a018c2a2f66a5c
Shreenivasanmech <35688877+Shreenivasanmech@users.noreply.github.com>
2018-02-09 14:09:22 +0530

    shreei10

---
+++ shreei10.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<copnio.h>
+n=int(raw_input("Enter the number"))
+temp=abs(n)
+count=0
+while(temp!=0):
+  temp=int(temp/10)
+  #print(temp)
+  count=count+1
+print (count)
+return o;
+}
