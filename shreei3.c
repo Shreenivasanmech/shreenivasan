4fb5ee7816bee1bb41b4b9038ad495749eb1fcf7
Shreenivasanmech <35688877+Shreenivasanmech@users.noreply.github.com>
2018-02-09 13:46:09 +0530

    shreei3.c

---
+++ shreei3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char c;
+printf("enter the character");
+scanf("%c",&c);
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+{
+printf("it is vowel");
+}
+else
+{
+printf("it is not vowel");
+return 0;
+getch()
+}
+}
