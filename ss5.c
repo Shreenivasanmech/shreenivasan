74447af2a1aad2b511d3d2b0cac3937cb70a9d66
Shreenivasanmech <35688877+Shreenivasanmech@users.noreply.github.com>
2018-02-21 16:15:51 +0530

    ss5.c

---
+++ ss5.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<Conio.h>
+int main()
+{
+int n,max=0,i,a[10];
+max=a[0];
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+printf("\n %d %d",a[i],i);
+}
+return 0;
+ghetch();
+}
