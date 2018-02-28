9d5e08f165dab7a1aa26d01247f8aaf794315ff1
Shreenivasanmech <35688877+Shreenivasanmech@users.noreply.github.com>
2018-02-28 16:00:42 +0530

    s38.c

---
+++ s38.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int a,b;
+  printf("Enter the two elements:");
+  scanf("%d\t%d",&a,&b);
+  a^=b;
+  b^=a;
+  a^=b;
+  printf("After swapping %d %d",a,b);
+  getch()
+  return 0;
+}
