b0084ae9117e38411c006efb4658dc7aa1d01b3b
Shreenivasanmech <35688877+Shreenivasanmech@users.noreply.github.com>
2018-03-02 13:23:29 +0530

    s43.c

---
+++ s43.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char s[10],s1[10];
+    scanf("%s",s);
+    scanf("%s",s1);
+    printf("%s%s",s,s1);
+    
+
+    return 0;
+    getch()
+}
