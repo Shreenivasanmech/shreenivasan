635321387bf0a0f8b9b42a78b4e728dca521ef43
Shreenivasanmech <35688877+Shreenivasanmech@users.noreply.github.com>
2018-03-02 13:41:39 +0530

    Update 11.c

--- 11.c
+++ 11.c
@@ -2,12 +2,12 @@
 #include<conio.h>
 int main()
 {
-	char str[10000],len,i,j,temp;
+char str[10000],len,i,j,temp;
 	scanf("%s",str);
 	len=strlen(str);
 	i=0;
 	j=len-1;
-	while(i<j)
+	while(i.j)
 	{
 		temp=str[i];
 		str[i]=str[j];
