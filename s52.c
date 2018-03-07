9a77afb99d9a4f50d16c1960042562fda4f114e8
Shreenivasanmech <35688877+Shreenivasanmech@users.noreply.github.com>
2018-03-07 15:59:14 +0530

    Update s52.c

--- s52.c
+++ s52.c
@@ -2,45 +2,46 @@ include<stdio.h>;
 include<conio.h>
 int main(void) 
 {
-	int num;
-	printf("\n enter the number from 1 to 10");
-	scanf("%d",&num);
-	switch(num)
-	{
+int num;
+printf("\n enter the number from 1 to 10");
+scanf("%d",&num);
+switch(num)
+{
 		case 1:
-		printf("\none");
-		break;
+	printf("\none");
+	break;
 		case 2:
-		printf("\ntwo");
-		break;
+	printf("\ntwo");
+	break;
 		case 3:
-		printf("\nthree");
+	printf("\nthree");
 		break;
-		case 4:
+	case 4:
 		printf("\nfour");
 		break;
-		case 5:
+	case 5:
 		printf("\nfive");
-		break;
+	break;
 		case 6:
 		printf("\nsix");
-		break;
+	break;
 		case 7:
 		printf("\nseven");
 		break;
-		case 8:
+	case 8:
 		printf("\neight");
-		break;
+	break;
 		case 9:
-		printf("\nnine");
+	printf("\nnine");
 		break;
 		case 10:
-		printf("\nten");
+	printf("\nten");
 		break;
-		default:
+	default:
 		printf("\n enter numbers from 1 to 10");
 	}
- 
+
 	return 0;
+	getch();
 }
  
