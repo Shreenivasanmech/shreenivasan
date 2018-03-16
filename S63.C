bdd13f5cfce3c572bab97d2bc09e459bc68dcdaa
Shreenivasanmech <35688877+Shreenivasanmech@users.noreply.github.com>
2018-03-16 15:48:08 +0530

    Create S63.C

---
+++ S63.C
@@ -0,0 +1,41 @@
+#include <stdio.h>
+ INCLUDE<CONIO.H>
+
+int main(void) 
+
+{
+
+	char str[50];
+
+	int i,count=1;
+
+	printf("enter the sentence:");
+  
+
+
+	scanf("%[^\n]s",str);
+  
+
+
+	for(i=0;str[i]!='\0';i++)
+
+	{
+
+		if(str[i]==' ')
+
+		{
+
+			count=count+1;
+
+		}
+
+	}
+
+	printf("\nno of words=%d",count);
+  
+  GETCH();
+  
+
+	return 0;
+
+}
